68bda1144e35e6c854056eaa0eae175989396ab8
Cesar <cesar.j.pardo.barrientos@gmail.com>
2024-08-01 00:17:45 -0400

    seccion de equipo

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const equipos = [{
+    name: "equipo1",
+    image: "club1.webp",
+},{
+    name: "equipo2",
+    image: "club2.jpg",
+},{
+    name: "equipo3",
+    image: "club3.jpg",
+},{
+    name: "equipo4",
+    image: "club4.jpg",
+},{
+    name: "equipo5",
+    image: "club1.webp",
+},{
+    name: "equipo6",
+    image: "club4.jpg",
+},{
+    name: "equipo7",
+    image: "club3.jpg",
+},{
+    name: "equipo8",
+    image: "club2.jpg",
+},
+]
